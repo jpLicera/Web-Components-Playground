73365d24ba68a771b49d8b42d8730a92c67a15b7
Licera <juanlicera@outlook.com>
2025-04-23 13:34:31 -0300

    Added the initial version of the `textual-throbber` element.

---
+++ src/textual-throbber/TextualThrobber.ts
@@ -0,0 +1,24 @@
+export class TextualThrobber extends HTMLElement {
+
+  public connectedCallback() {
+    console.log("Custom element added to page.");
+  }
+
+  public disconnectedCallback() {
+    console.log("Custom element removed from page.");
+  }
+
+  public connectedMoveCallback() {
+    console.log("Custom element moved with moveBefore()");
+  }
+
+  public adoptedCallback() {
+    console.log("Custom element moved to new page.");
+  }
+
+  public attributeChangedCallback(name: any, oldValue: any, newValue: any) {
+    console.log(`Attribute ${name} has changed.`);
+  }
+}
+
+customElements.define("textual-throbber", TextualThrobber);
