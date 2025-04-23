fbcb6a5f9faca6681a009a871aff7bfbfb407de5
Licera <juanlicera@outlook.com>
2025-04-23 20:26:42 -0300

    Added the `registerComponents` function.

---
+++ src/registerComponents.ts
@@ -0,0 +1,11 @@
+import {COMPONENTS} from "./data/Components";
+
+/**
+ * Registers all the web components defined in `COMPONENTS`
+ * @see COMPONENTS
+ */
+export function registerComponents(): void {
+  for (const component of COMPONENTS) {
+    customElements.define(component.name, component.constructor);
+  }
+}
