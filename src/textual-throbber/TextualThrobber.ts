e11383199c0c4cb06c150b955cd1d8a3bc30661b
Licera <juanlicera@outlook.com>
2025-04-24 14:00:51 -0300

    Primitive first version of the `textual-throbber` element that uses a shadow DOM.

--- src/textual-throbber/TextualThrobber.ts
+++ src/textual-throbber/TextualThrobber.ts
@@ -1,7 +1,28 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    .dot { animation: blink 1s infinite; }
+    @keyframes blink {
+        0%, 100% { opacity: 0; }
+        50% { opacity: 1; }
+    }
+  </style>
+  <p class="dot">I blink</p>
+  <div class="wrapper">
+    <slot></slot> 
+  </div>
+`;
+
 export class TextualThrobber extends HTMLElement {
 
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+  }
+
   public connectedCallback() {
-    console.log("Custom element added to page.");
+    console.log("Custom element added to page");
+    this.shadowRoot!.appendChild(template.content.cloneNode(true));
   }
 
   public disconnectedCallback() {
