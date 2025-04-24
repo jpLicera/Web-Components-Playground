426a9231d3fd8e9b122ca0f03bb6f56775c645d2
Licera <juanlicera@outlook.com>
2025-04-24 17:39:00 -0300

    Added multiple missing JSDocs. Fixed a couple of CSS declarations.

--- src/textual-throbber/TextualThrobber.ts
+++ src/textual-throbber/TextualThrobber.ts
@@ -1,5 +1,9 @@
-const template = document.createElement('template');
-template.innerHTML = `
+/**
+ * `template` element used to populate the shadow DOM.
+ */
+const ELEMENT_TEMPLATE: HTMLTemplateElement = document.createElement('template');
+
+ELEMENT_TEMPLATE.innerHTML = `
   <div class="loading-wrapper loading-wrapper--hidden">
     <div class="spinner"></div>
     <p id="message"></p>
@@ -8,7 +12,7 @@ template.innerHTML = `
   <style>
     .loading-wrapper {
       --accent-color: #00ffcc;
-      position: absolute;
+      position: fixed;
       top: 0;
       left: 0;
       right: 0;
@@ -22,7 +26,7 @@ template.innerHTML = `
       background-color: rgba(0, 0, 0, 0.5);
       color: var(--accent-color);
       font-size: 1.2rem;
-      opacity: 1000%;
+      opacity: 100%;
       transition: opacity 0.5s ease-in-out;
     }
     
@@ -76,15 +80,35 @@ export class TextualThrobber extends HTMLElement {
     }, this.intervalMs);
   }
 
+  /**
+   * Lifecycle method that is called when this element is added to the document's DOM.
+   * Appends a clone of this template's template to the shadow DOM.
+   * @see ELEMENT_TEMPLATE
+   */
   public connectedCallback(): void {
-    this.shadowRoot!.appendChild(template.content.cloneNode(true));
+    this.shadowRoot!.appendChild(ELEMENT_TEMPLATE.content.cloneNode(true));
   }
 
+  /**
+   * Lifecycle method invoked when this element is disconnected from the DOM.
+   * Performs cleanup tasks or updates element state, such as stopping ongoing processes.
+   *
+   * @return {void} Does not return any value.
+   */
   public disconnectedCallback(): void {
     this.toggleLoading(false);
   }
 
-  public attributeChangedCallback(name: any, oldValue: any, newValue: any): void {
+  /**
+   * Method invoked whenever one of the observed attributes changes.
+   *
+   * @param {string} name - The name of the attribute that was changed.
+   * @param {string} oldValue - The previous value of the attribute before the change.
+   * @param {string} newValue - The new value of the attribute after the change.
+   *
+   * @see observedAttributes
+   */
+  public attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
     console.log(`Attribute ${name} has changed: ${oldValue} -> ${newValue}`);
     this.toggleLoading(newValue === "true");
   }
